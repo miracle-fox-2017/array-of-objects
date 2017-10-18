93809aee6d8bcfd75fe1a72adb08b51ffa83e5d5
alang <alangmahendra@gail.com>
2017-10-18 21:07:58 +0700

    sakaratul

---
+++ arrObb.js
@@ -0,0 +1,17 @@
+var arr =[];
+function arrObj(name,phase,gender) {
+ let hasil ={
+   name  : name,
+   phase : phase,
+   gender: gender,
+ }
+  arr.push(hasil)
+}
+
+arrObj('akbar',1,'male')
+arrObj('alang',1,'male')
+console.log(arr)
+
+function getData(name) {
+  
+}
